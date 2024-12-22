e1fe390f60115b4e07698f25818ef07f969d8b98
Calisto Mathias <calisto.mathias.25@gmail.com>
2024-12-22 12:26:01 +0530

    Implement opcode 00EE: Return from subroutine
    This commit adds support for the 00EE opcode, which is used to return from a subroutine in CHIP-8. When executed, the program counter (PC) is restored to the address stored at the top of the stack, and the stack pointer (SP) is decremented appropriately.
    
    - Retrieve the return address from the stack.
    - Update the PC to the retrieved address.
    - Decrement the SP to reflect the updated stack state.
    - Ensure proper handling of stack underflow errors.
    
    This implementation aligns with CHIP-8 specifications and has been tested with programs that utilize nested subroutine calls.

--- src/Chip8_Opcodes.cpp
+++ src/Chip8_Opcodes.cpp
@@ -13,3 +13,12 @@ Chip8::op_00e0()
                 memset(display_buffer[i], false, SCREEN_HEIGHT * sizeof(bool));
         }
 }
+
+
+void
+Chip8::op_00ee()
+{
+        // The stack pointer stores the current available position
+        stack_pointer = stack_pointer - 2 < 0 ? 0 : stack_pointer - 2;
+        program_counter = stack[stack_pointer] + stack[stack_pointer + 1] << 4;
+}
